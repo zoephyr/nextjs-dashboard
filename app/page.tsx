320443d13cb448bb74bb7cc35ca7b9ed88761437
Zoey <38047633+zoephyr@users.noreply.github.com>
2024-01-06 01:59:32 -0500

    Zoey in 2024 lol ecksdee

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Page() {
             <a href="https://nextjs.org/learn/" className="text-purple-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , brought to you by Vercel. And Zoey :^)
             
           </p>
           <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent">
